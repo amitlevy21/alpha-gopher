8b510e385e0f4051b5e0ff21f8af883ad5aea893
Amit Levy <Amit.Levy1@ibm.com>
2019-05-29 21:11:13 +0300

    get all users

---
+++ api/filesystem.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"net/http"
+	"os/exec"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getAllUsers(c *gin.Context) {
+
+	users, err := exec.Command("awk -F: '{ print $1}' /etc/passwd").Output()
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": err})
+	}
+	c.JSON(200, gin.H{"message": users})
+}
---
+++ test/filesystem_test.go
@@ -0,0 +1,27 @@
+package test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", getAllUsers)
+	return r
+}
+
+func TestPingRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "pong", w.Body.String())
+}
