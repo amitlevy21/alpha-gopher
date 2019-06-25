e8585ef8effde654a59f060daf5c118dd2b5583d
Amit Levy <Amit.Levy1@ibm.com>
2019-06-25 19:21:14 +0300

    add slide menu and router dep

--- web/alpha-gopher-frontend/src/App.vue
+++ web/alpha-gopher-frontend/src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
+    <Slide>
+      <a id="home" href="#">
+        <span>Home</span>
+      </a>
+      <a id="file-system" href="#">
+        <span>File System</span>
+      </a>
+      <a id="user-management" href="#users">
+        <span>User</span>
+      </a>
+      <a id="system" href="#">
+        <span>System</span>
+      </a>
+    </Slide>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Slide } from "vue-burger-menu";
 
 export default {
-  name: 'app',
+  name: 'AlphaGopher',
   components: {
-    HelloWorld
+    Slide
   }
 }
 </script>
--- web/alpha-gopher-frontend/src/main.js
+++ web/alpha-gopher-frontend/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import VueResource from "vue-resource"
+import VueRouter from "vue-router"
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App),
