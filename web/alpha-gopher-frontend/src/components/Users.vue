f208bd369b75a8c06e43224f608b5c1895e50467
Amit Levy <amitlevy347@gmail.com>
2019-06-27 00:26:57 +0300

    create add user button

--- web/alpha-gopher-frontend/src/components/Users.vue
+++ web/alpha-gopher-frontend/src/components/Users.vue
@@ -1,12 +1,47 @@
 <template>
   <div id="users">
     <h1>Users</h1>
+    <div id="users-list">
+      <ul>
+        <li v-for="user in users">
+          {{ user }}
+        </li>
+      </ul>
+    </div>
+    <input type="text" v-model="newUserName" required /> 
+    <button @:click="addUser">
+      Add User
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Users'
+  name: 'Users',
+  data() {
+    return {
+      users: [],
+      newUserName: ''
+    }
+  },
+  created() {
+    this.$http.get('users/all').then(function(data){
+            this.users = data.body
+        }).catch(function (error) {
+          console.log(error);
+          
+          console.log("failed to get users" + error.body)
+        });
+  },
+  methods: {
+    addUser: function () {
+      this.$http.post('users/new/' + this.newUserName).then(function (data) {
+
+      }).catch(function (error) {
+        console.log("failed to add user error:" + error)
+      })
+    }
+  }
 }
 </script>
 
