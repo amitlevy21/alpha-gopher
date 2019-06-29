b628078d278eae48d01d8f8c69ddc9a2db7b60dc
Amit Levy <amitlevy347@gmail.com>
2019-06-29 20:49:36 +0300

    add terminal ui

---
+++ web/alpha-gopher-frontend/src/components/Terminal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Terminal</h1>
+    <app-terminal :intro="intro"
+                console-sign="$"
+                allow-arbitrary
+                height="500px"
+                @command="onCliCommand"/>
+  </div>
+</template>
+
+<script>
+import VueTerminal from "vue-terminal-ui";
+
+export default {
+  components: {
+    'app-terminal': VueTerminal
+  },
+  methods: {
+      onCliCommand (data, resolve, reject) {
+        console.log(data.text)
+        resolve('')
+      }
+  }
+};
+</script>
+
+<style>
+</style>
--- web/alpha-gopher-frontend/src/routes.js
+++ web/alpha-gopher-frontend/src/routes.js
@@ -1,11 +1,13 @@
 import Users from './components/Users.vue'
 import FileSystem from './components/FileSystem.vue'
 import System from './components/System.vue'
+import Terminal from './components/Terminal.vue'
 import Help from './components/Help.vue'
 
 export default [
     {path: '/users', component: Users},
     {path: '/filesystem', component: FileSystem},
     {path: '/system', component: System},
+    {path: '/terminal', component: Terminal},
     {path: '/help', component: Help}
 ]
