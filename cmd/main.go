8f5cfe1e716f97fcb56e000f0ded6847df7345ae
Amit Levy <amitlevy347@gmail.com>
2019-07-05 18:34:11 +0300

    define initial backend api

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,15 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/devices", api.GetDevices)
+	router.POST("/backup", api.Backup)
+	router.GET("/date", api.GetDate)
+	router.POST("/date/:newDate", api.SetDate)
+	router.POST("/filesystem/cp", api.Copy)
+	router.POST("/filesystem/ls", api.List)
+	router.POST("/filesystem/mv", api.Move)
+	router.GET("/owner", api.GetOwner)
+	router.POST("/owner/:newOwner", api.ChangeOwnership)
+	router.GET("/permission", api.GetCurrentPermissions)
 
-	router.Run()
+	router.Run(":3000")
 }
