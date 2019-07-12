758d53f194de8a3fadb517710a4fcd1e44821f7b
Amit Levy <amitlevy347@gmail.com>
2019-07-12 14:01:45 +0300

    HTop fix

---
+++ web/alpha-gopher-frontend/start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+./web/alpha-gopher-frontend/vue_htop.sh &
+http-server dist
--- web/alpha-gopher-frontend/vue_htop.sh
+++ web/alpha-gopher-frontend/vue_htop.sh
@@ -5,5 +5,5 @@ do
     ./htop_to_html.sh ./public/htop.html
     ./html_to_vue.sh ./public/htop.html ./src/components/HTop.vue
     ./node_modules/eslint/bin/eslint.js ./src/components/HTop.vue --fix 
-	sleep 8
+	sleep 6
 done
