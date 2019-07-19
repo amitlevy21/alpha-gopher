61b29b69b185c667d105e9d7188dfb5dbbea75a5
Amit Levy <amitlevy347@gmail.com>
2019-07-19 15:32:15 +0300

    fix not running cd command

--- internal/terminal.go
+++ internal/terminal.go
@@ -1,12 +1,25 @@
 package internal
 
 import (
+	"os"
 	"os/exec"
 	"strings"
 )
 
+var currentDir = os.Getenv("PROJECT_PATH")
+
 func ExecCommand(command []string) (string, error) {
-	combinedStd, err := exec.Command(command[0], command[1:]...).CombinedOutput()
+	if command[0] == "cd" {
+		if len(command) > 1 {
+			currentDir = command[1]
+		} else {
+			currentDir = "~"
+		}
+		return currentDir, nil
+	}
+	cmd := exec.Command(command[0], command[1:]...)
+	cmd.Dir = currentDir
+	combinedStd, err := cmd.CombinedOutput()
 	s := string(combinedStd)
 	sTrimmed := strings.TrimSuffix(s, "\n")
 	return sTrimmed, err
