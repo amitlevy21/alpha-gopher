2ffd0c5c4d516f1495051bec8b30e0cb0c3e644f
Amit Levy <Amit.Levy1@ibm.com>
2019-07-30 19:03:18 +0300

    add help

--- web/alpha-gopher-frontend/src/components/Help.vue
+++ web/alpha-gopher-frontend/src/components/Help.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
     <h1>Help</h1>
-    <b />
+    <h2>Scripts</h2>
+    <h4>File Browser</h4>
+    <p>Using tree command for fetching the hirrarchy of the file system</p>
+    <h4>File Finder</h4>
+    <p>Using find command with name and date options</p>
+    <h4>Users</h4>
+    <p>Displays users info and enables adding and removing users, using useradd and userdl.</p>
+    <h4>Devices</h4>
+    <p>Using lsblk command for fetching info about each devices, the script shows devices that are connected to host of the container</p>
+    <h4>System info</h4>
+    <p>Using htop command and then piping it to aha util that is reponsible of converting a terminal program output to html output</p>
+    <h4>Backup</h4>
+    <p>Using rsync command for syncing the filesystem to the /backup while ignoring some files such as /dev and /proc</p>
+    <h4>Terminal</h4>
+    <p>Enable remote execution of commands into the container</p>
+    
   </div>
 </template>
 
